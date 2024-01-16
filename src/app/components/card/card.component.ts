649ef540725fbd38e3eae16d72186419d2e43097
Chinmay Patankar <chinmay.patankar@cvshealth.com>
2024-01-16 12:57:47 -0500

    used reactive forms to initialize task

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, SimpleChanges } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-card',
@@ -6,13 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.scss'],
 })
 export class CardComponent implements OnInit {
-  cannotEdit: boolean = true;
+  @Input() value: any;
+  task = new FormControl({ value: '', disabled: true });
   constructor() {}
 
   ngOnInit(): void {}
 
+  ngOnChanges(changes: SimpleChanges): void {
+    //Called before any other lifecycle hook. Use it to inject dependencies, but avoid any serious work here.
+    //Add '${implements OnChanges}' to the class.
+    this.task.setValue(changes.value.currentValue);
+  }
+
   editTask() {
-    this.cannotEdit = false;
+    this.task.enable();
     console.log('opened modal');
+  }
+
+  disableEdit() {
+    this.task.disable();
   }
 }
