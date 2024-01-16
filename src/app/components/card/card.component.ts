fa07451c8b0835e36fd17a47ef4d0a6e2de835df
Chinmay Patankar <chinmay.patankar@cvshealth.com>
2024-01-16 12:30:46 -0500

    change to input ele; feat: enable/disable card edit

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -3,14 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+  styleUrls: ['./card.component.scss'],
 })
 export class CardComponent implements OnInit {
+  cannotEdit: boolean = true;
   constructor() {}
 
   ngOnInit(): void {}
 
   editTask() {
-    console.log('opened modal')
+    this.cannotEdit = false;
+    console.log('opened modal');
   }
 }
